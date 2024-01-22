b2df1e3c713f34c6de8a2a645b30d002806c1f1a
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2024-01-22 23:04:08 +0900

    feat: Split mean_deficiency_correction.py
    Splits the 'mean_deficiency_correction' file into 'example' file to
    improve code structure.

--- mean_deficiency_correction.py
+++ mean_deficiency_correction.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 def median_absolute_deviation(data):
     """
@@ -129,33 +126,4 @@ def log_mean(data):
     original_scale_value = np.exp(log_mean_value)
     return log_mean_value, original_scale_value
 
-
-
-
-# Load the 'tips' dataset in Seaborn
-tips_data = sns.load_dataset('tips')
-
-# Visualization
-sns.histplot(tips_data['total_bill'], bins='auto', kde=True)
-plt.title('Distribution of Total Bill Amounts')
-plt.xlabel('Total Bill Amount')
-plt.ylabel('Frequency')
-plt.show()
-
-
-tips_data = tips_data['total_bill']
-# Calculate each metric
-mean_value = np.mean(tips_data)
-median_value = np.median(tips_data)
-mad_value = median_absolute_deviation(tips_data)
-trimmed_mean_value = trimmed_mean(tips_data, tail='right')
-log_mean_result = log_mean(tips_data)
-
-# Results ------------
-print(f"Original Mean: {mean_value}")
-print(f"Median: {median_value}")
-print(f"MAD: {mad_value}")
-print(f"Trimmed Mean: {trimmed_mean_value}")
-print(f"Log Mean: {log_mean_result[0]}")
-print(f"Original Scale Mean: {log_mean_result[1]}")
 
